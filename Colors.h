ef7ecaec5763da478e03e37d8fbef44d61fa7b46
Taha Azzaoui <tazzaoui@cs.uml.edu>
2018-02-18 23:03:19 -0500

    Added Color Codes

---
+++ Colors.h
@@ -0,0 +1,16 @@
+/* 
+ *  
+ *   Color Codes for Printing 
+ *   Status Messages To The Terminal
+ *
+*/
+
+const char* RESET = "\e[0m";
+const char* BLACK = "\e[0;30m";
+const char* RED = "\e[0;31m";
+const char* GREEN = "\e[0;32m";
+const char* YELLOW = "\e[0;33m";
+const char* BLUE = "\e[0;34m";
+const char* PURPLE = "\e[0;35m";
+const char* CYAN = "\e[0;36m";
+const char* WHITE = "\e[0;37m";
