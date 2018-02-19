b096cdcd2ca306e4f4cf57d9f2a91616bbee22ee
Taha Azzaoui <tazzaoui@cs.uml.edu>
2018-02-19 00:30:09 -0500

    Implemented Connection Function

--- Connect.c
+++ Connect.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 #include "Connect.h"
 
 int create_socket(void){
   int sock = socket(AF_INET, SOCK_STREAM, 0);
-  if( sock  >= 0)
+  if(sock  >= 0)
     printf("[+] Socket Created Successfully.\n");
   else{
     perror("create_socket");
@@ -39,7 +38,7 @@ int listen_for_connection(int listener_socket, int backlog){
   if(status == 0)
     printf("[+] Listening...\n");
   else{
-    perror("Listen");
+    perror("listen_for_connection");
     exit(1);
   }
   return status;
@@ -47,12 +46,23 @@ int listen_for_connection(int listener_socket, int backlog){
 
 int accept_connection_from_client(int server_socket,struct sockaddr* client,
 				  socklen_t* addr_size){
-  int client_socket = accept(server_socket, (struct sockaddr*) &client, addr_size);
+  int client_socket = accept(server_socket,(struct sockaddr*)&client,addr_size);
   if(client_socket >= 0)
     printf("[+] Accepted Connection.\n");
   else{
-    perror("Accept");
+    perror("accept_connection_from_client");
     exit(1);
   }
   return client_socket;
+}
+
+int connect_to_server(int client_socket, struct sockaddr* server){
+  int status = connect(client_socket,server,sizeof(*server));
+  if(status == 0)
+    printf("[+] Address successfully bound to socket.\n");
+  else{
+    perror("connect_to_server");
+    exit(1);
+  }
+  return status;
 }
