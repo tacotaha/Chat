0d62fad4c625b0be33bda5cdd1dab93dfcc54b6d
Taha Azzaoui <tazzaoui@cs.uml.edu>
2018-02-23 16:55:41 -0500

    Implemented concurrency across multiple clients

--- Server.c
+++ Server.c
@@ -16,8 +16,8 @@
 #define BACKLOG 5
 
 int main(){
-  int server_socket,client_socket, bytes_received;
-  struct sockaddr_in server, client;
+  int server_socket;
+  struct sockaddr_in server;
   char out_buffer[BUF], in_buffer[BUF];
   socklen_t addr_size;
   
@@ -37,54 +37,28 @@ int main(){
 
   /*Listen For Connections*/
   listen_for_connection(server_socket, BACKLOG);
-  
-  /*Accept a connection from the client*/
-  client_socket = accept_connection_from_client(server_socket,(struct sockaddr*)&client,&addr_size);
+
   
   /*=======================================Main Loop========================================*/
   while(1){
-    /*Receive Data From Client*/
-    bytes_received = recv(client_socket, in_buffer, sizeof(in_buffer), 0);
-    if(bytes_received < 0){
-      perror("Recv");
-      exit(1);
-    }else if(bytes_received == 0){
-      printf("[-] Client Disconnected\n");
-      exit(1);
+    struct sockaddr_storage client_addr;
+    int client_socket;
+    
+    /*Try accepting any incoming connections, skip to next iteration if none*/
+    if((client_socket = accept(server_socket, (struct sockaddr*) &client_addr, &addr_size)) < 0){
+      perror("accept()");
+      continue;
+    }
+
+    /* Spawn a thread to handle the client that just connected*/
+    pthread_t client_thread;
+    pthread_attr_t client_thread_attr;
+    pthread_attr_init(&client_thread_attr);
+    if((pthread_create(&client_thread, &client_thread_attr, p_server_to_clients, &client_socket)) < 0){
+      perror("pthread_create()");
+      pthread_exit(0);
     }
     
-    /*Print Client Response To stdout*/
-    in_buffer[BUF - 1] = '\0';
-    printf("Client: %s\n", in_buffer);
-    fflush(stdout);
-    memset(in_buffer, '\0',sizeof(in_buffer));
-
-    /*Read Response From Server (stdin)*/
-    printf(">> ");
-    fgets(out_buffer,BUF,stdin);
-    fflush(stdout);
-    out_buffer[BUF - 1] = '\0';
-    
-    /*Send Response To Client
-    bytes_sent = send(client_socket, out_buffer, sizeof(out_buffer), 0);
-    if(bytes_sent < 0){
-      perror("Send");
-      exit(1);
-    }
-    */
-
-    Message m;
-    m.to_socket = client_socket;
-    m.from_socket = server_socket;
-    m.message = out_buffer;
-    
-    pthread_t tid;
-    pthread_attr_t attr;
-    pthread_attr_init(&attr);
-
-    pthread_create(&tid, &attr, p_server_to_clients, &m);
-    pthread_join(tid,NULL);
-
     /*Reset Buffers*/
     memset(out_buffer, '\0', sizeof(out_buffer));
     memset(in_buffer, '\0',sizeof(in_buffer));
@@ -93,6 +67,5 @@ int main(){
   
   printf("[-] Closing the connection\n");
   close(server_socket);
-  close(client_socket);
   return 0;
 }
