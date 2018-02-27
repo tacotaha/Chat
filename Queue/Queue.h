d96fa47d678d707548a24abed95c4e5b4ebcadc9
Taha Azzaoui <tazzaoui@cs.uml.edu>
2018-02-27 17:23:21 -0500

    Generic Queue

--- Queue/Queue.h
+++ Queue/Queue.h
@@ -4,16 +4,27 @@
 #include "Status.h"
 
 typedef void* QUEUE;
+typedef struct node Node;
+
+struct node{
+  void* data;
+  Node* next;
+  Node* prev;
+};
+
+typedef struct queue{
+  Node* head;
+  Node* tail;
+  int size;
+}Queue;
+
 
 QUEUE queue_init(void);
-Status queue_enqueue(QUEUE hQueue, int data);
+Status queue_enqueue(QUEUE hQueue, void* data);
 Status queue_dequeue(QUEUE hQueue);
 Boolean queue_empty(QUEUE hQueue);
 int queue_size(QUEUE hQueue);
-int queue_front(QUEUE hQueue);
+void* queue_front(QUEUE hQueue);
 void queue_destroy(QUEUE* phQueue);
-void queue_print(QUEUE hQueue);
-
-
 
 #endif
