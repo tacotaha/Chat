446f0cb44b04d7bc8852de70f8c5a093ac30cc68
Taha Azzaoui <tazzaoui@cs.uml.edu>
2018-03-14 02:30:05 -0400

    Added error checking

--- Stream.c
+++ Stream.c
@@ -4,24 +4,35 @@
 #include "Stream.h"
 #include "Connect.h"
 
-
 void* p_server_to_clients(void* arg){
+  Message* m = (Message*) arg;
   int  bytes_sent = 0;
-  char out_buffer[] = "Hello there! This is a concurrency test!";
-
   int client_socket = *(int*)arg;
-
-  /*Send Response To Client*/
-  bytes_sent = send(client_socket, out_buffer, sizeof(out_buffer), 0);
-  if(bytes_sent < 0){
-    perror("DID NOT SEND!!");
-    pthread_exit(0);
-  }else{
-    printf("IT ACTUAL SENT YEEEE!!\n");
+  char in_buffer[BUF];
+  
+  int bytes_received = recv(client_socket, in_buffer, sizeof(in_buffer), 0);
+  if(bytes_received < 0){
+    perror("Recv");
+    exit(1);
+  }else if(bytes_received == 0){
+    printf("[-] Client Disconnected\n");
+    exit(1);
   }
 
+  
+  bytes_sent = send(client_socket,m->message, sizeof(m->message), 0);
+  if(bytes_sent < 0){
+    perror("Send()");
+    pthread_exit(0);
+  }
+  
   pthread_detach(pthread_self());
   printf("Socket %d connected!\n", client_socket);
-   
+ 
+  free(m);
   pthread_exit(0);
 }
+
+/*Spawn thread to send a message 
+  from a client to the server.*/
+void* p_client_to_server(void* arg);
