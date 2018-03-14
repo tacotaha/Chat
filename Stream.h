396f01cbfe2b93b63fdc5e4425bcbc889e12205b
Taha Azzaoui <tazzaoui@cs.uml.edu>
2018-03-14 02:29:32 -0400

    Added Stream

--- Stream.h
+++ Stream.h
@@ -1,17 +1,36 @@
 #ifndef __STREAM_H__
 #define __STREAM_H__
 
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/select.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <pthread.h>
+#include "Queue/Queue.h"
+
 #define BUF 1024
+#define MAX_CLIENTS 100
+
+typedef struct client{
+  QUEUE* messages;
+  pthread_mutex_t* mutex;
+  int sender;
+  int* clients;
+}Client;
 
 typedef struct message{
-  int to_socket;
-  int from_socket;
-char* message;
+  char* message;
+  int sender;
 }Message;
 
-/*Spawn thread to send a message 
-  from a client to the server.*/
-void* p_client_to_server(void* arg);
+typedef struct chat{
+  fd_set listeners;
+  int socket;
+  int clients[MAX_CLIENTS];
+  pthread_mutex_t *clients_m;
+}Chat;
 
 /*Spawn thread to output the server's
  buffer to the each client.
@@ -20,4 +39,5 @@ void* p_client_to_server(void* arg);
  untill it's been written out, to prevent
  other theads from overwriting it.*/
 void* p_server_to_clients(void* arg);
+
 #endif
