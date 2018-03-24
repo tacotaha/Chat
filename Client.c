8c8a60415b2cd46e60dfc6f8b87c01048653b0ba
Taha Azzaoui <tazzaoui@cs.uml.edu>
2018-03-24 18:59:05 -0400

    Multi-threaded Client

--- Client.c
+++ Client.c
@@ -1,22 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "Connect.h"
+#include <pthread.h>
+#include <ctype.h>
+#include <time.h>
 
-#define PORT 4444
-#define IP "127.0.0.1"
-#define BUF 1024
+
+#include "Colors.h"
+#include "Connect.h"
+#include "Stream.h"
 
 int main(){
-  int client_socket, bytes_sent, bytes_received;
+  srand(time(0));
+  
+  int client_socket;
   struct sockaddr_in server_addr;
-  char in_buffer[BUF], out_buffer[BUF];
+  pthread_t read, write;
+  Message m;
+  char buffer[BUF];
+  const char* color = COLORS[rand() % num_colors];
   
-  /*Clear Repsective Buffers*/
   memset(&client_socket,0,sizeof(client_socket));
-  memset(in_buffer,0,sizeof(in_buffer));
-  memset(out_buffer,0,sizeof(out_buffer));
   
+  printf("Username: ");
+  fgets(buffer,sizeof(buffer),stdin);
+  
+  for(size_t i = 0; i < sizeof(buffer); ++i)
+    if(isspace(buffer[i]))
+      m.user_name[i] = 0;
+    else
+      m.user_name[i] = buffer[i];
+
   /*Create Client Endpoint For Communcation*/
   client_socket = create_socket();
   
@@ -26,41 +40,23 @@ int main(){
   /*Connect To Server*/
   connect_to_server(client_socket,(struct sockaddr*)&server_addr);
   
-  /*=======================================Main Loop========================================*/
-  while(1){
-    printf(">> ");
-    fgets(out_buffer, sizeof(out_buffer), stdin);
-    fflush(stdout);
-    in_buffer[BUF - 1] = '\0';
-    
-    /* Initialize Conversation With Server*/
-    bytes_sent = send(client_socket, out_buffer, sizeof(out_buffer),0);
-    if(bytes_sent < 0){
-      perror("Send");
-      exit(1);
-    }
-    
-    /*Read Response From Server*/
-    bytes_received = recv(client_socket, in_buffer, sizeof(in_buffer), 0);
-    if(bytes_received < 0){
-      perror("Recv");
-      exit(1);
-    }else if(bytes_received == 0){
-      printf("[-] Server Disconnected\n");
-      exit(1);
-    }
-    
-    /*Print Server Response To stdout*/
-    in_buffer[BUF - 1] = '\0';
-    printf("Server: %s\n", in_buffer);
-    fflush(stdout);
-    
-    /*Reset Buffers*/
-    memset(out_buffer, '\0', sizeof(out_buffer));
-    memset(in_buffer, '\0',sizeof(in_buffer));
+  m.socket = client_socket;
+  
+  for(size_t i = 0; i < COLOR_SIZE; ++i)
+    m.color[i] = color[i];
+  
+  if(pthread_create(&read, NULL, client_read_thread, &client_socket) < 0){
+    perror("pthread_create");
+    return 1;
   }
   
-  printf("[-] Closing the connection.\n");
-  close(client_socket);
+  if(pthread_create(&write, NULL, client_write_thread, &m) < 0){
+    perror("pthread_create");
+    return 1;
+  }
+    
+  pthread_join(read,NULL);
+  pthread_join(write,NULL);
+  
   return 0;
 }
