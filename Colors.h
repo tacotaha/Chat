3d15f5a495e72cab2673145820493033c2ef64a5
Taha Azzaoui <tazzaoui@cs.uml.edu>
2018-03-25 00:20:58 -0400

    Broke Const correctness

--- Colors.h
+++ Colors.h
@@ -12,6 +12,6 @@
 static const int num_colors = 6;
 
 typedef enum color_index{RESET,RED,GREEN,YELLOW,BLUE,PURPLE,CYAN} Color_Index;
-static const char* COLORS[] = {"\033[0m","\033[1;31m","\033[1;32m","\033[1;33m","\033[1;34m","\033[1;35m","\033[1;36m"};
+static char* COLORS[] = {"\033[0m","\033[1;31m","\033[1;32m","\033[1;33m","\033[1;34m","\033[1;35m","\033[1;36m"};
 
 #endif
